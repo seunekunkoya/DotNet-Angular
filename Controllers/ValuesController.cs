62d99650356f91c29cfa5fb4e45b4e768edf09a5
ekunkoya <ekunkoya.seun@gmail.com>
2021-07-07 19:20:08 +0000

    added dotnet environment variables

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ollyekun.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        public ValuesController(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        [HttpGet]
+        [Route("getAppSettings")]
+        public async Task<string> GetappSettings()
+        {
+            var appsettings = _config["name"];
+            return appsettings;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,16 +11,24 @@ namespace ollyekun
 {
     public class Program
     {
+        static string envName;
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            envName = string.IsNullOrEmpty(config["configEnvName"]) ? "development" : config["configEnvName"];
+
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
+            .ConfigureAppConfiguration((x, config) =>
+            {
+                config.AddJsonFile("appsettings.json").AddJsonFile($"appsettings.{envName}.json");
+            })
+            .ConfigureWebHostDefaults(webBuilder =>
+             {
                     webBuilder.UseStartup<Startup>();
-                });
+             });
     }
 }
